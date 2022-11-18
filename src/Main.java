1d8a3e1c6e49ec49db46abe619697d450a548c14
Edward-Kazan-Skypro <zvezda_torgi@mail.ru>
2022-11-18 22:30:39 +0300

    Домашняя работа 18 ноября СлучайныеЧисла

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.util.HashSet;
+
 public class Main {
+
+    static HashSet<Integer> numbersSet = new HashSet<>();
+
+    public static void fillSet() {
+        while (numbersSet.size() < 20) {
+            int randomNumber = (int) (Math.random() * 1001 - 1);
+            numbersSet.add(randomNumber);
+        }
+    }
+
+    public static void viewSet() {
+        System.out.println(numbersSet);
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        fillSet();
+        viewSet();
     }
 }
